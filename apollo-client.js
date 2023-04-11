4f025b803143e7ef5dad99f9ed89cf8a8c20e01d
brettdavidbowles <brett.bowles@gmail.com>
2023-04-10 21:28:21 -0400

    uri thing

--- apollo-client.js
+++ apollo-client.js
@@ -7,12 +7,13 @@ import {
 const client = new ApolloClient({
   ssrMode: true,
   link: createHttpLink({
-    // uri: 'http://localhost:8000/graphql/',
-    uri: 'http://ec2-18-188-62-39.us-east-2.compute.amazonaws.com/graphql/'
+    uri: 'http://localhost:8000/graphql/',
+    // uri: 'http://ec2-18-188-62-39.us-east-2.compute.amazonaws.com/graphql/'
     // credentials: 'same-origin',
     // headers: {
     //   cookie: req.header('Cookie'),
     // },
+    headers: {}
   }),
   cache: new InMemoryCache(),
 });
