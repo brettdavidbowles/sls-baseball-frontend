64754fe776d04d6aa6b3cb3bac8617164cdd2823
brettdavidbowles <brett.bowles@gmail.com>
2023-05-27 22:44:29 -0400

    useMemo for subtitle in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import { useRouter } from 'next/router'
 import Hamburger from "./Hamburger"
+import { useMemo } from "react"
 
 interface Props {
   toggleSlider: Function,
@@ -13,22 +14,24 @@ export default function Header(props: Props) {
     props.toggleSlider()
   }
   const router = useRouter()
+  const hideSubtitle = useMemo(() => {
+    return props.collapsed && props.isMobile
+  }, [props.collapsed, props.isMobile])
+
   const content = () => {
     return (
-      <div className="w-full">
-        <div className="relative w-full">
-          <div className={`py-5 z-20 transition-translate duration-500 -mx-24 absolute ${props.collapsed ? 'translate-x-24' : 'translate-x-1/2'} w-full`}>
+      <div className="relative w-full">
+        <div className={`py-5 z-20 transition-translate duration-500 -mx-24 absolute ${props.collapsed ? 'translate-x-24' : 'translate-x-1/2'} w-full`}>
 
-            <h1><Link href="/">Baseball Simulator</Link></h1>
+          <h1><Link href="/">Baseball Simulator</Link></h1>
 
-          </div>
-          <Hamburger handleClick={toggleSlider} />
-          <div className={`fixed top-0 left-0 w-full transition-all duration-500 ${props.collapsed ? 'h-20' : 'h-36'} header-gradient`} />
-          <div className={`fixed top-0 left-0 transition-all duration-500 ${props.collapsed ? 'pt-7' : 'pt-24'} w-full`}>
-            <p className={`transition-all duration-500 ${props.collapsed && props.isMobile ? 'opacity-0' : 'opacity-100'} w-full text-center`}>
-              An interactive baseball simulator.
-            </p>
-          </div>
+        </div>
+        <Hamburger handleClick={toggleSlider} />
+        <div className={`fixed top-0 left-0 w-full transition-all duration-500 ${props.collapsed ? 'h-20' : 'h-36'} header-gradient`} />
+        <div className={`fixed top-0 left-0 transition-all duration-500 ${props.collapsed ? 'pt-7' : 'pt-24'} w-full`}>
+          <p className={`transition-all duration-500 ${hideSubtitle ? 'opacity-0' : 'opacity-100'} w-full text-center`}>
+            An interactive baseball simulator.
+          </p>
         </div>
       </div>
     )
