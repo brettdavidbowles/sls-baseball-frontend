42583e12ee1a7aa29f345ec1cfc6d3c3fee25cb1
brettdavidbowles <brett.bowles@gmail.com>
2023-07-13 20:05:42 -0400

    updatd DateTime component to get rid of production error

--- src/components/DateTime.tsx
+++ src/components/DateTime.tsx
@@ -1,14 +1,38 @@
 import { dateObj } from "types/types";
 import { formatDateTime } from "utils/formatDateTime";
+import { useEffect, useState } from "react";
 
 export default function DateTime({ dateTime }: { dateTime: string }) {
-  const { date, time, timezone }: dateObj = formatDateTime(dateTime)
-  return (
-    <div className="flex flex-col py-2">
-      <div>
-        {date}
+  const [dateObj, setDateObj] = useState<dateObj>({ date: '', time: '', timezone: '' })
+  useEffect(() => {
+    setDateObj(formatDateTime(dateTime))
+  }, [dateTime])
+
+  const { date, time, timezone }: dateObj = dateObj
+
+  const dateTimeComponent = () => {
+    if (!dateObj?.date) {
+      return (
+        <div className="flex flex-col py-2">
+          <div className="h-6" />
+          <div className="h-6" />
+        </div>
+      )
+    }
+    return (
+
+      <div className="flex flex-col py-2">
+        <div className="h-6">
+          {date}
+        </div>
+        <div className="h-6">
+          {`${time} (${timezone})`}
+        </div>
       </div>
-      <div>{`${time} (${timezone})`}</div>
-    </div>
+    )
+  }
+
+  return (
+    dateTimeComponent()
   );
 }
