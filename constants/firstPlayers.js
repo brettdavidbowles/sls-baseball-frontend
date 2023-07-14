0a928eebc2cf006e32535cdcb50b37e86fe5f5e0
brettdavidbowles <brett.bowles@gmail.com>
2023-07-14 00:56:15 -0400

    players

--- constants/firstPlayers.js
+++ constants/firstPlayers.js
@@ -2,143 +2,104 @@
 
 export const players = [
   {
-    firstName: "stanley",
-    lastName: "hauerwas",
-    team: "people of the cloth"
-  },
-  {
-    firstName: "athanasius",
-    lastName: "alexandria",
-    team: "people of the cloth"
-  },
-  {
-    firstName: "gregory",
-    lastName: "nyssa",
-    team: "people of the cloth"
-  },
-  {
-    firstName: "gregory",
-    lastName: "nazianzus",
-    team: "people of the cloth"
-  },
-  {
-    firstName: "basil",
-    lastName: "caesarea",
-    team: "people of the cloth"
-  },
-  {
-    firstName: "maximus",
-    lastName: "confessor",
-    team: "people of the cloth"
-  },
-  {
-    firstName: "augustine",
-    lastName: "hippo",
-    team: "people of the cloth"
-  },
-  {
     firstName: "john",
-    lastName: "chrysostom",
-    team: "people of the cloth"
+    lastName: "rockefeller",
+    team: "titans of industry"
   },
   {
-    firstName: "origen",
-    lastName: "alexandria",
-    team: "people of the cloth"
+    firstName: "cornelius",
+    lastName: "vanderbilt",
+    team: "titans of industry"
   },
   {
-    firstName: "irenaeus",
-    lastName: "smyrna",
-    team: "people of the cloth"
+    firstName: "andrew",
+    lastName: "carnegie",
+    team: "titans of industry"
   },
   {
-    firstName: "ignatius",
-    lastName: "antioch",
-    team: "people of the cloth"
+    firstName: "j.p.",
+    lastName: "morgan",
+    team: "titans of industry"
+  },
+  {
+    firstName: "henry",
+    lastName: "ford",
+    team: "titans of industry"
+  },
+  {
+    firstName: "andrew",
+    lastName: "mellon",
+    team: "titans of industry"
   },
   {
     firstName: "james",
-    lastName: "cone",
-    team: "people of the cloth"
+    lastName: "duke",
+    team: "titans of industry"
   },
   {
-    firstName: "gustavo",
-    lastName: "gutierrez",
-    team: "people of the cloth"
+    firstName: "henry",
+    lastName: "flagler",
+    team: "titans of industry"
   },
   {
-    firstName: "david bentley",
-    lastName: "hart",
-    team: "people of the cloth"
+    firstName: "george",
+    lastName: "pullman",
+    team: "titans of industry"
   },
   {
-    firstName: "rowan",
-    lastName: "williams",
-    team: "people of the cloth"
+    firstName: "james",
+    lastName: "hill",
+    team: "titans of industry"
   },
   {
-    firstName: "john scotus",
-    lastName: "eriugena",
-    team: "people of the cloth"
+    firstName: "henry",
+    lastName: "frick",
+    team: "titans of industry"
   },
   {
-    firstName: "iamblichus",
-    lastName: "chalcidensis",
-    team: "people of the cloth"
+    firstName: "charles",
+    lastName: "crocker",
+    team: "titans of industry"
   },
   {
-    firstName: "dionysius",
-    lastName: "areopagite",
-    team: "people of the cloth"
+    firstName: "leland",
+    lastName: "stanford",
+    team: "titans of industry"
+  },
+  {
+    firstName: "collis",
+    lastName: "huntington",
+    team: "titans of industry"
+  },
+  {
+    firstName: "edward",
+    lastName: "harriman",
+    team: "titans of industry"
+  },
+  {
+    firstName: "henry",
+    lastName: "rogers",
+    team: "titans of industry"
+  },
+  {
+    firstName: "charles",
+    lastName: "pratt",
+    team: "titans of industry"
+  },
+  {
+    firstName: "joseph",
+    lastName: "pulitzer",
+    team: "titans of industry"
   },
   {
     firstName: "thomas",
-    lastName: "aquino",
-    team: "people of the cloth"
+    lastName: "edison",
+    team: "titans of industry"
   },
   {
-    firstName: "henri",
-    lastName: "de lubac",
-    team: "people of the cloth"
+    firstName: "larry",
+    lastName: "page",
+    team: "titans of industry"
   },
-  {
-    firstName: "hans urs von",
-    lastName: "balthasar",
-    team: "people of the cloth"
-  },
-  {
-    firstName: "karl",
-    lastName: "barth",
-    team: "people of the cloth"
-  },
-  {
-    firstName: "dorothy",
-    lastName: "sayers",
-    team: "people of the cloth"
-  },
-  {
-    firstName: "sarah",
-    lastName: "coakley",
-    team: "people of the cloth"
-  },
-  {
-    firstName: "michael",
-    lastName: "ramsey",
-    team: "people of the cloth"
-  },
-  {
-    firstName: "richard",
-    lastName: "hooker",
-    team: "people of the cloth"
-  },
-  {
-    firstName: "sergei",
-    lastName: "bulgakov",
-    team: "people of the cloth"
-  },
-  {
-    firstName: "abraham",
-    lastName: "kuyper",
-    team: "people of the cloth"
-  }
+
 ]
