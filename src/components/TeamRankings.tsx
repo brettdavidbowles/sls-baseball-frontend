e517428f51c51e9377c3c505fd105164d0539354
brettdavidbowles <brett.bowles@gmail.com>
2023-07-26 21:17:38 -0400

    sorted rankings

--- src/components/TeamRankings.tsx
+++ src/components/TeamRankings.tsx
@@ -1,15 +1,20 @@
 import { Season, TeamRanking } from "types/gqlTypes"
 import { removeUnderscore } from "utils/removeUnderscore"
+import { useMemo } from 'react'
 
 
 export default function TeamRankings({ season }: { season: Season }) {
 
+  const rankings = useMemo(() => {
+    if (!season?.rankings) return []
+    return [...season.rankings].sort((a: TeamRanking, b: TeamRanking) => a.rank - b.rank)
+  }, [season.rankings])
 
   return (
     <div>
       <h2 className="capitalize text-xl">{removeUnderscore(season.name)} Rankings</h2>
       {
-        season.rankings?.map((ranking: TeamRanking) => (
+        rankings?.map((ranking: TeamRanking) => (
           <div
             key={ranking.team.id}
             className="flex justify-between max-w-sm mx-auto px-8"
