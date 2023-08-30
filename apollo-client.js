a087d4857eb8a605105793efc7a727107a3e4ef4
brettdavidbowles <brett.bowles@gmail.com>
2023-08-30 19:47:28 -0400

    credential -> credentials

--- apollo-client.js
+++ apollo-client.js
@@ -7,7 +7,7 @@ import {
 const httpLink = createHttpLink({
   uri: process.env.NEXT_PUBLIC_API_URL,
   // credentials: process.env.NEXT_PUBLIC_ENV === 'development' ? 'include' : 'same-origin',
-  credential: 'include'
+  credentials: 'include'
 })
 
 const client = new ApolloClient({
